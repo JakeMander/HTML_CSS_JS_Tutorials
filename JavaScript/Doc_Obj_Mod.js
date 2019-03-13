2f8952b2c188570a0635c6a846bd8ed6b69fb9f8
Jake Mander <jmmander1@hotmail.co.uk>
2019-03-13 19:35:45 +0000

    Added DOM Exercise
    Introduction to the Document Object Model and how to edit/change nodes with JavaScript.

---
+++ JavaScript/Doc_Obj_Mod.js
@@ -0,0 +1,16 @@
+/*  The Document Object Model (DOM) Defines The Structure Of A Website In Terms Of A Tree Structure. HTML Elements Are
+ *  Broken Up Into Linked Nodes Which Branch Of Into Other Connected Nodes.
+ *
+ *  The Root Of The Tree Is The "Document" Node As Such The Document Node Can Be Used To Access All Other Nodes.
+ *  All Other Nodes Become Child Nodes And "Sprout" From This And Nodes On The Same Level Are Known As Siblings.
+ *
+ *  An Example Of A Node Could Be The <html> Tag. This Becomes Its Own Node Linked To The Document Parent Node.
+ *  From This Will Sprout The Head and Body Nodes, Which Will Contain All The Webpage Meta-data as Well As All The
+ *  Elements Contained Within The HTML File Such as <h1>, <p>, <em> and <image> tags.
+ *
+ *  From This, Nodes Which Contain Text Or Have Particular Attributes Are Then Assigned "Attribute" or "Text" Nodes
+ *  Respectively. These Contain The Text And Attributes Used By Their Respective Tags To Be Displayed Or Behave.
+ *
+ *  A User Can Access Nodes And Change Their Contents Using Functions Such As innerHTML. For Example, Adding 
+ *
+*/
